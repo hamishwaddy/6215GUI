d8859450c86cfdb33451bfcc3091e45b0d0dee3b
Hamish Wadsworth <9835077@student.toiohomai.ac.nz>
2019-03-21 16:51:55 +1300

    Week4 stuff

---
+++ ManualButton/ManualButton/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManualButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void My_Handler(object sender, RoutedEventArgs e)
+        //{
+        //    var btn = new Button();
+        //    btn.Content = "Click Me Again!";
+        //    btn.Background = Brushes.Red;
+        //    btn.FontSize = 30;
+
+        //    MyGrid.Children.Add(btn);
+        //}
+
+        private void My_Handler(object sender, RoutedEventArgs e)
+        {
+            Label label = new Label();
+            label.Content = txtbox.Text;
+            label.FontSize = 30;
+            
+            MyGrid.Children.Add(label);
+        }
+    }
+}
